4e1d683cdb9e4ed98c4ce39ee7fcfb9f167ca4a1
jmcui <jmcui00@gmail.com>
2020-02-26 12:47:47 +0800

    兼容ustcthesis3.2, 添加了English模版。添加了release脚本。
     chapter没有英文模版，后续改进

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+rm -R ./release/*
+cp *.lyx ./release/
+cp *.md ./release/
+cp *.bib ./release/
+
+cp ./ustcthesis/ustcthesis-*.bst ./release/
+cp ./ustcthesis/ustcthesis.cls ./release/
+cp -R ./ustcthesis/figures ./release/
+
+
+cp -R ./chapters ./release/
+rm ./release/chapters/*.lyx~
+cp ./ustcthesis/ustcthesis.cls ./release/chapters/
+cp ./USTCthesis.layout ./release/chapters/
